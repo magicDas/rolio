dc49b43fc7e6d5870dfb558c4974414e77e2e20b
Peter Tor <magicdas@i.ua>
2023-04-05 10:09:02 +0300

    files3

---
+++ src/js/01_main.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function () {
+    let $header = document.querySelector('.header'),
+    top = this.scrollY;
+    if (top>0) {
+        $header.classList.add('--scroll');
+    } else {
+        $header.classList.remove('--scroll');
+    }
+})
+
+function round() {
+    let $round = document.querySelector('header__round'),
+    wrapperLeft = document.querySelector('.header__wrapper').offsetLeft;
+
+    return $round.style.left = wrapperLeft + 'px';
+}
+round();
